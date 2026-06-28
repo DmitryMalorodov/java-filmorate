12c11cae77965cda30a3ca23a3e34479cee335be
Nepovezlo <sasasuharevskij98@gmail.com>
2026-06-28 16:32:54 +0300

    Добавить аннотацию @JsonAlias("reviewId")

--- src/main/java/ru/yandex/practicum/filmorate/model/review/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/review/Review.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.model.review;
 
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -10,6 +11,7 @@ import lombok.Data;
 @Data
 @Builder(toBuilder = true)
 public class Review {
+    @JsonAlias("reviewId")
     private Long id;
 
     @NotBlank(message = "Содержание отзыва не может быть пустым")
